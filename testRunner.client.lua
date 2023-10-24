76448fe039071c1541aa9b193f28fd51c2f78126
Casey <103451081+dev-syn@users.noreply.github.com>
2023-10-24 05:50:47 -0400

    Delay the testRunner from running

--- testRunner.client.lua
+++ testRunner.client.lua
@@ -1,2 +1,6 @@
+for i = 4,1 do
+    warn(`Tests will begin in {i} seconds.`)
+    task.wait(1);
+end
 local TestEZ = require(script.Parent.Packages.TestEZ);
 TestEZ.TestBootstrap:run({ script.Parent.Packages.TypeWriter });
